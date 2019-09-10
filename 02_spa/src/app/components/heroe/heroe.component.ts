5f4c8e8bca0cfc0fbefd7db7e4b80d1c2d3232ad
Julio cesar <cesar.languasco@gmail.com>
2019-09-10 17:09:47 -0500

    aplicacion web server

---
+++ 02_spa/src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+//---para obtener el parametro que viene por el url de la pagina ejm heroe/id
+import { ActivatedRoute } from '@angular/router';
+import { HeroesServices, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+})
+export class HeroeComponent {
+ 
+  heroe:any={};
+
+  constructor(private _activarteRoute:ActivatedRoute,private _heroesService:HeroesServices) { 
+    //--- propiedad que se obtiene por el url debe de ser igual al que se definio en el app.routes.
+    this._activarteRoute.params.subscribe( param =>{
+      this.heroe= this._heroesService.getHeroe(param['id'])
+      console.log(this.heroe);
+    })
+  } 
+}
--- 02_spa/src/app/components/heroes/heroes.component.ts
+++ 02_spa/src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesServices, Heroe } from '../../services/heroes.service';
+//--- sirve para navegar hacia otras paginas
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-heroes',
+  selector: 'app-heroes',  
   templateUrl: './heroes.component.html',
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit { 
 
-  constructor() { }
+  heroes:Heroe [] = [];
+
+  constructor(private _heroesService:HeroesServices, private _router:Router ) {
+    console.log('constructor');
+   }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+  }
+  
+  verHeroe(index:number){
+    ///---navegar hacia una nueva pagina parecido a un link
+     this._router.navigate(['/heroe', index])
   }
 
 }
