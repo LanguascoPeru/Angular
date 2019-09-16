8f7c55c2c60a38538f093b7db3c46b3da11d8c39
Languasco <cesar.languasco@gmail.com>
2019-09-16 18:22:00 -0500

    ejemplos de pipes

---
+++ 03_pipes/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre:string= "Julio Cesar";
+  arreglo:number [] = [1,2,3,4,5,6,7,8,9,10];
+  PI:number=3.1415926535;
+  ax:number= 0.234;
+  salario:number =12345;
+}
