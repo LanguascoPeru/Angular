2a24546813812e162aebe62138ee719e7ebe9898
Languasco <cesar.languasco@gmail.com>
2019-09-27 18:43:01 -0500

    cambios en el sistema

---
+++ 07_micelaneos/src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <p [ngStyle]="{'font-size': '15px'}">
+       Etiqueta con stylo dinamico
+    </p>
+    <p [ngStyle]="{'font-size': tamanio + 'px'}">
+      Etiqueta con stylo dinamico
+   </p>
+   <p [style.fontSize.px]="tamanio">
+      Etiqueta con stylo dinamico
+   </p>
+
+   <button class="btn btn-primary" (click)="tamanio= tamanio + 5"> 
+      <i class="fa fa-plus"></i>
+   </button>
+
+   <button class="btn btn-danger" (click)="tamanio= tamanio - 5">
+       <i class="fa fa-minus"></i>
+   </button>
+
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  tamanio:number= 20;
+  tamanioPx:string= "'{font-size':40px}'";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
