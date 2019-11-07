367fa1c05ceb23cb04c9778b7a6c9908e70b604a
Languasco <cesar.languasco@gmail.com>
2019-11-07 18:35:52 -0500

    cambios varis

--- calidda/src/app/pages/home/home.component.ts
+++ calidda/src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
-import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
 import { LoginService } from '../../services/login.service';
  
 
@@ -10,34 +9,17 @@ import { LoginService } from '../../services/login.service';
 })
 export class HomeComponent implements OnInit {
   
-  
-  @ViewChild('labelImport')
-  labelImport: ElementRef;
-
-  formImport: FormGroup;
-  fileToUpload: File = null;
-
   constructor(private loginService:LoginService ) { 
-    this.formImport = new FormGroup({
-      importFile: new FormControl('', Validators.required)
-   });
+ 
   }
 
   ngOnInit() { 
     this.loginService.updateLoginStatus(true);
   }
  
+ 
 
-  onFileChange(files: FileList) {
-    this.labelImport.nativeElement.innerText = Array.from(files)
-      .map(f => f.name)
-      .join(', ');
-    this.fileToUpload = files.item(0);
-  }
 
-  import(): void {
-    console.log('import ' + this.fileToUpload.name);
-  }
 
 
 }
